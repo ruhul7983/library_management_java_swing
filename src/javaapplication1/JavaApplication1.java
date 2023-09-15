e9cbf501e3dfe84a8217eb3ecc06c49a3924b144
User <ruhulamin7983@gmail.com>
2023-09-16 01:35:27 +0600

    First Commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JavaApplication1 {
+
+    void connection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_management", "root", "RuhuLAmiN4$");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from students");
+            int count = 0;
+            while (resultSet.next()) {
+                System.out.println(count);
+                System.out.println(resultSet.getString("StudentId"));
+                System.out.println(resultSet.getString("name"));
+                System.out.println(resultSet.getString("department"));
+                System.out.println("password");
+                count++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        JavaApplication1 j = new JavaApplication1();
+        j.connection();
+        FirstPage.main(args);
+    }
+    
+}
